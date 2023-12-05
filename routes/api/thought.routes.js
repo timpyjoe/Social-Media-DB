d753bf8f3e053668b7f75ab2b0ebb7e7b96bff9f
Tim Poffenberger <timpoffenberger23@gmail.com>
2023-12-04 20:56:40 -0600

    Thoughts now get pushed to their respective user's thoughts array

--- routes/api/thought.routes.js
+++ routes/api/thought.routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Thought } = require('../../models');
+const { Thought, User } = require('../../models');
 const Model = Thought; 
 
 
@@ -24,7 +24,23 @@ router.get("/:id", async (req, res) => {
 router.post("/" , async (req, res) => {
   try {
     const payload = await Model.create(req.body);
+    const thoughtObject = await User.findById(req.body.userID, {'thoughts':1, "_id":0})
+    thoughtObject.thoughts.push(payload._id)
+    const user = await User.findByIdAndUpdate(req.body.userID, {thoughts: thoughtObject.thoughts})
     res.status(200).json({ result: "success", payload})
+  } catch (err) {
+    res.status(500).json({ result: "error", payload: err.message})
+  }
+})
+
+router.post("/:thoughtId/reactions", async (req, res) => {
+  try {
+    const thoughtObject = await Model.findById(req.params.thoughtId, {'reactions':1, "_id":0})
+    const bodyArray = [req.body]
+    console.log(req.body)
+    thoughtObject.reactions.push(bodyArray)
+    const payload = await Model.findByIdAndUpdate(req.params.thoughtId, {reactions: thoughtObject.reactions})
+    res.status(200).json({resutl: "success", payload })
   } catch (err) {
     res.status(500).json({ result: "error", payload: err.message})
   }
